a5dcc85f7938a634827cf8ce12e5bee10514623e
Nuha Al-Sameai <noha.pepeep1@gmail.com>
2020-09-21 04:10:13 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ const fi = (function() {
       return 'Start by reading https://medium.com/javascript-scene/master-the-javascript-interview-what-is-functional-programming-7f218c68b3a0'
     },
 
-    each: function() {
-
+    each: function(collection,callback) {
+      if(Array.isArray(collection)){
+     collection.forEach(element => callback(element))
+      }
+      else{
+       for (const property in collection) {
+  callback(collection[property]);
+}}
+      return collection;
     },
 
-    map: function() {
-
+    map: function(collection,callback) {
+    const a={collection.map(element=>callback(element))}
     },
 
     reduce: function() {
